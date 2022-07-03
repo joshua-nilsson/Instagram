74879ef03315256f4035a8c7ca0d3bab177fea47
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2022-07-03 17:46:48 -0600

    Firebase Init

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCBpRfjMXI0uQ8obfVsUkGDy5jMzorakUY',
-  authDomain: 'instagram-yt.firebaseapp.com',
-  projectId: 'instagram-yt',
-  storageBucket: 'instagram-yt.appspot.com',
-  messagingSenderId: '763311512694',
-  appId: '1:763311512694:web:fc66f0787342163c791357'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 
 const firebase = Firebase.initializeApp(config);
