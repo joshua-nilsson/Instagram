7f4c54d0c810dfa0721c490990196e7b16dfa6e9
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2023-05-31 12:47:51 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
 import './styles/app.css';
+import './styles/style.css';
 
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
