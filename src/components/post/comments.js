918f5a0732b2e1c18f9934023dcf8fcabb9c8ce4
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2023-05-31 00:27:01 -0600

    Update comments.js

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -6,13 +6,11 @@ import AddComment from './add-comment';
 
 export default function Comments({ docId, comments: allComments, posted, commentInput }) {
   const [comments, setComments] = useState(allComments);
+  const [viewAllComments, setViewAllComments] = useState(false);
 
   return (
     <>
       <div className="p-4 pt-1 pb-4">
-        {comments.length >= 3 && (
-          <p className="text-sm text-gray-base mb-1 cursor-pointer">View all comments</p>
-        )}
         {comments.slice(0, 3).map((item) => (
           <p key={`${item.comment}-${item.displayName}`} className="mb-1">
             <Link to={`/p/${item.displayName}`}>
@@ -21,6 +19,24 @@ export default function Comments({ docId, comments: allComments, posted, comment
             <span>{item.comment}</span>
           </p>
         ))}
+        {!viewAllComments && comments.length >= 3 && (
+          <button
+            type="button"
+            className="text-sm text-gray-base mb-1 cursor-pointer"
+            onClick={() => setViewAllComments(true)}
+          >
+            View all comments
+          </button>
+        )}
+        {viewAllComments &&
+          comments.slice(3, comments.length).map((item) => (
+            <p key={`${item.comment}-${item.displayName}`} className="mb-1">
+              <Link to={`/p/${item.displayName}`}>
+                <span className="mr-1 font-bold">{item.displayName}</span>
+              </Link>
+              <span>{item.comment}</span>
+            </p>
+          ))}
         <p className="text-gray-base uppercase text-xs mt-2">
           {formatDistance(posted, new Date())} ago
         </p>
