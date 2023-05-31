8edb3bf010699cfc6c3bed5dfcc50051042acd70
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2023-05-31 00:28:54 -0600

    Update use-photos.js

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -7,11 +7,8 @@ export default function usePhotos(user) {
   useEffect(() => {
     async function getTimelinePhotos() {
       // example: [2, 1, 5] <- 2 being raphel
-      let followedUserPhotos = [];
-
-      // does the user actually follow people?
       if (user?.following?.length > 0) {
-        followedUserPhotos = await getPhotos(user.userId, user.following);
+        const followedUserPhotos = await getPhotos(user.userId, user.following);
         // re-arrange array to be newest photos first by dateCreated
         followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
         setPhotos(followedUserPhotos);
